a1b7effd1c1f5c299d31ba7d9d5da421ba677e6f
tronldau <kazuo.tenri@gmail.com>
2024-06-20 00:33:41 +0700

    demo

---
+++ main/Main.java
@@ -0,0 +1,18 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args){
+        JFrame window = new JFrame("Tetris");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+
+        GamePanel gp = new GamePanel();
+        window.add(gp);
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+}
